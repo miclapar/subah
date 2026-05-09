29c891f95e1eb7996ad028e60e647e466b350802
miclapar <joicibela0@gmail.com>
2026-05-10 06:14:09 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,9 @@
+self.addEventListener('install', (e) => {
+      e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['index.html'])));
+    });
+    self.addEventListener('fetch', (e) => {
+      e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    });
+    ```
 
+**Selesai!** Sekarang website Anda sudah memiliki fitur lengkap: Portal informasi, Agenda terbaru, dan Aplikasi Mobile yang bisa diinstal orang tua.
